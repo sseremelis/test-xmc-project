4f5d46dd3e0c57dc56e5afc2032871b17ff80386
Sotiris Seremelis <sssotiris22@hotmail.com>
2025-05-19 17:03:32 +0300

    Update ByocContent.tsx

--- headapps/nextjs-starter/src/components/ByocContent.tsx
+++ headapps/nextjs-starter/src/components/ByocContent.tsx
@@ -1,38 +1,36 @@
-import React from 'react';
-import * as FEAAS from '@sitecore-feaas/clientside/react';
+import React from 'react'
+import * as FEAAS from '@sitecore-feaas/clientside/react'
 
 interface ByocContentProps {
-  title: string;
-  complex: {
-    label: string;
-    href: string;
-  };
+  title: string
+  complex: {
+    label: string
+    href: string
+  }
 }
 
 export const ByocContent = (props: ByocContentProps): JSX.Element => {
-  return (
-    <div className="container">
-      <h2>{props.title || 'BYOC Demo'}</h2>
-      <p>ByocContent Component</p>
-      <div>{props.title}</div>
-      <div>{props.complex?.label}</div>
-    </div>
-  );
-};
+  return (
+    <div className='container'>
+      <h2>{props.title || 'BYOC Demo'}</h2>
+      <p>ByocContent Component</p>
+      <div>{props.title}</div>      <div>{props.complex?.label}</div>
+    </div>
+  )
+}
 
 FEAAS.External.registerComponent(ByocContent, {
-  name: 'ByocContent',
-  properties: {
-    title: {
-      type: 'string',
-    },
-    complex: {
-      type: 'object',
-      properties: {
-        label: { type: 'string'},
-        href: { type: 'string'},
-      },
-    },
-
-  },
-});
+  name: 'ByocContent',
+  properties: {
+    title: {
+      type: 'string'
+    },
+    complex: {
+      type: 'object',
+      properties: {
+        label: { type: 'string' },
+        href: { type: 'string' }
+      }
+    }
+  }
+})
